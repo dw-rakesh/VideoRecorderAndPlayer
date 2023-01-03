60109433f5446414aa85ffc6d05f1c064bab7af3
Rakesh Kota <rakesh.kota@mutualmobile.com>
2023-01-03 17:23:36 +0530

    Initial Commit

---
+++ VideoRecorderAndPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VideoRecorderAndPlayer
+//
+//  Created by Rakesh Kota on 03/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
